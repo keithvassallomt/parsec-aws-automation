19bc833f8cfbb1bbfbb89e16da019402de569176
Cookie-Monster-Coder <53286741+Cookie-Monster-Coder@users.noreply.github.com>
2020-11-17 02:51:35 -0600

    Update SnapAndDelete.py

--- SnapAndDelete.py
+++ SnapAndDelete.py
@@ -96,7 +96,8 @@ def lambda_handler(object, context):
             DryRun=False, 
             VirtualizationType='hvm', 
             EnaSupport=True #Supported instance types: current generation instance type, other than C4, D2, M4 instances smaller than m4.16xlarge, or T2. https://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/enhanced-networking-ena.html
-            #SriovNetSupport='simple' Supported instance types: C3, C4, D2, I2, M4 (excluding m4.16xlarge), and R3. https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/sriov-networking.html
+            #Remove # in froont of "SriovNetSupport='simple'" to use SriovNetSupport, and add a # in front of "EnaSupport=True".
+            #SriovNetSupport='simple' #Supported instance types: C3, C4, D2, I2, M4 (excluding m4.16xlarge), and R3. https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/sriov-networking.html
         )
         print('Created image {}'.format(ami['ImageId']))
         amis_created.append(ami['ImageId'])
